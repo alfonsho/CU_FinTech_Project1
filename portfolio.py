591ca621b0279271b534c1482b08a1fcd0938f0b
Alfonso Salces <alfonso@Alfonsos-MBP.fios-router.home>
2022-05-14 12:17:31 -0400

    wrote some scaffolding at portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -3,6 +3,20 @@ import pandas as pd
 
 
 class portfolio:
+    """
+    portfolio may have functionality and attributes:
+        tickers - attr
+        weights - attr
+        dollar value - attr
+        covariance
+        sharpe ratio
+        volatility
+        historic_prices
+        date when position was taken 
+        take position
+        plot
+
+    """
     def __init__(self, tickers):
 
         pass
